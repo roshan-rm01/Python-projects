f724e069a1fa4287c24f1b53a59dad4e970d9f66
roshan-rm01 <63264453+roshan-rm01@users.noreply.github.com>
2021-06-24 22:16:44 +0100

    Delete Generic.py

--- Generic.py
+++
@@ -1,9 +0,0 @@
-names = ["Boris", "Justin", "Vladimir", "Xi", "Donald", "Angela", "Emmanuel"]
-
-paragraph = f"Dear {name}" \
-            f"During these desperate times it is now apparent to look" \
-            f"into the future of science and to collaborate to fend off this apparent threat"
-
-for name in names:
-
-    
